f060d65540ea9fdf0b0ce842d4c314b9c42cbd19
Mark Wolfe <mark.wolfe@versent.com.au>
2020-04-17 21:57:27 +1000

    feat(attributes) Add sub command to list attributes

---
+++ pkg/commands/list_attributes.go
@@ -0,0 +1,36 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/jedib0t/go-pretty/table"
+	"github.com/rs/zerolog/log"
+)
+
+// ListAttributesCmd list pools attributes sub command
+type ListAttributesCmd struct {
+	UserPoolID string `help:"User pool id." kong:"required"`
+}
+
+// Run run the list operation
+func (f *ListAttributesCmd) Run(ctx *Context) error {
+
+	log.Debug().Msg("list attributes")
+
+	attrs, err := ctx.Cognito.DescribePoolAttributes(f.UserPoolID)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to list pool attributes")
+	}
+
+	tw := table.NewWriter()
+
+	tw.AppendHeader(table.Row{"name"})
+
+	for _, att := range attrs {
+		tw.AppendRow(table.Row{att})
+	}
+
+	fmt.Fprintln(ctx.Writer, tw.Render())
+
+	return nil
+}
